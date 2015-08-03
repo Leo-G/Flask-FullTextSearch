a2b5ef136bda9e055e5476e87fc03288a3edc4f1
Leo <leo@leog.in>
2015-08-04 00:52:26 +0530

    ycombinator score

--- scraper.py
+++ scraper.py
@@ -11,27 +11,34 @@ msg = Message("Failed to add sites",
                   sender="leo@search.techarena51.com",
                   recipients=["leo.gonzalvez@gmail.com"])
 
+#headers = { 'user-agent': 'testing:v0.1 (by /u/12boy)' }
 
-site = requests.get("https://news.ycombinator.com/")
+#site = requests.get("https://news.ycombinator.com/", headers=headers)
 
-soup = BeautifulSoup(site.text, 'html.parser')
+soup = BeautifulSoup(open("html-test"), 'html.parser')
 
-for tag in soup.find_all(class_="title"):
-    if tag.a != None:
-        url = tag.a.get('href')
-        tag  =  tag.a.get_text()
-        content = "news.ycombintator.com, date: {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        with app.app_context():
-             from app.sites.models import Sites     
-             site=Sites(url, content, tag)
+for div in soup.find_all(class_="athing"):
+       #Ad links have no score on ycombinate eg: search for Zesty
+      if div.contents[4].a != None and div.next_sibling.span != None:
+          url = div.contents[4].a.get('href')
+          tag = div.contents[4].a.get_text()
+          ycombinator_score, string = div.next_sibling.span.get_text().split()
+          content = ""
+
+      with app.app_context():
+             from app.sites.models import Sites
+             site=Sites(url, content, tag, ycombinator_score=ycombinator_score)
              error = site.add(site)
              if error != None:
-                mail.send(msg)
-                break
-             
-              
-             
-    
-
-
-
+                msg.body =""" URL : {url},
+                              tag: {tag},
+                              score:{score},
+                              html : {div},
+                              ERROR = {error}""".format(url=url,
+                                                      tag=tag,
+                                                      score=ycombinator_score,
+                                                      div=div,
+                                                      error=error)
+                #mail.send(msg)
+                print(error)
+                continue
